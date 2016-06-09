b3275ab2bb259765db82e20260ef6a278fa44221
Xavi Aracil <xavi@laibeth.com>
2016-06-09 12:30:33 +0200

    Dependencies fetched from requirements.txt

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup_args = dict(
     author_email='anson@tophatmonocle.com',
     url='https://github.com/tophatmonocle/ims_lti_py',
     packages=find_packages(),
-    install_requires=['lxml', 'oauth2'],
     license='MIT License',
     keywords='lti',
     zip_safe=True,
