c6afccb822e1bf8263cd9505cfeb6bf65bfefd58
Xavi Aracil <xaracil@uoc.edu>
2018-03-07 14:28:41 +0100

    Back to pickle

--- ims_lti_py/tool_provider.py
+++ ims_lti_py/tool_provider.py
@@ -1,3 +1,5 @@
+from future import standard_library
+standard_library.install_aliases()
 from .launch_params import LaunchParamsMixin
 from .request_validator import (
     RequestValidatorMixin,
@@ -32,14 +34,14 @@ accessors = [
 
 
 class ToolProvider(LaunchParamsMixin, RequestValidatorMixin, object):
-    """
+    '''
     Implements the LTI Tool Provider.
-    """
+    '''
 
     def __init__(self, consumer_key, consumer_secret, params={}):
-        """
+        '''
         Create new ToolProvider.
-        """
+        '''
         # Initialize all class accessors to None
         for param in accessors:
             setattr(self, param, None)
@@ -57,48 +59,48 @@ class ToolProvider(LaunchParamsMixin, RequestValidatorMixin, object):
         self.process_params(params)
 
     def has_role(self, role):
-        """
+        '''
         Check whether the Launch Paramters set the role.
-        """
+        '''
         return self.roles and any([re.search(role, our_role, re.I)
                                    for our_role in self.roles])
 
     def is_student(self):
-        """
+        '''
         Convenience method for checking if the user has 'learner' or 'student'
         role.
-        """
+        '''
         return any((self.has_role('learner'),
                     self.has_role('student')))
 
     def is_instructor(self):
-        """
+        '''
         Convenience method for checking if user has 'instructor', 'faculty'
         or 'staff' role.
 
         Currently this does not support the TeachingAssistant role
-        """
+        '''
         return any((self.has_role('instructor'),
                     self.has_role('faculty'),
                     self.has_role('staff')))
 
     def is_launch_request(self):
-        """
+        '''
         Check if the request was an LTI Launch Request.
-        """
+        '''
         return self.lti_message_type == 'basic-lti-launch-request'
 
     def is_outcome_service(self):
-        """
+        '''
         Check if the Tool Launch expects an Outcome Result.
-        """
+        '''
         return (self.lis_outcome_service_url and
                 self.lis_result_sourcedid)
 
     def username(self, default=None):
-        """
+        '''
         Return the full, given, or family name if set.
-        """
+        '''
         if self.lis_person_name_given:
             return self.lis_person_name_given
         elif self.lis_person_name_family:
@@ -109,7 +111,7 @@ class ToolProvider(LaunchParamsMixin, RequestValidatorMixin, object):
             return default
 
     def post_replace_result(self, score, outcome_opts=defaultdict(lambda:None), result_data=None):
-        """
+        '''
         POSTs the given score to the Tool Consumer with a replaceResult.
 
         Returns OutcomeResponse object and stores it in self.outcome_request
@@ -121,41 +123,41 @@ class ToolProvider(LaunchParamsMixin, RequestValidatorMixin, object):
 
             'text' : str text
             'url' : str url
-        """
+        '''
         return self.new_request(outcome_opts).post_replace_result(score, result_data)
 
     def post_delete_result(self,outcome_opts=defaultdict(lambda:None)):
-        """
+        '''
         POSTs a delete request to the Tool Consumer.
-        """
+        '''
         return self.new_request(outcome_opts).post_delete_result()
 
     def post_read_result(self,outcome_opts=defaultdict(lambda:None)):
-        """
+        '''
         POSTs the given score to the Tool Consumer with a replaceResult, the
         returned OutcomeResponse will have the score.
-        """
+        '''
         return self.new_request(outcome_opts).post_read_result()
 
     def last_outcome_request(self):
-        """
+        '''
         Returns the most recent OutcomeRequest.
-        """
+        '''
         return self.outcome_requests[-1]
 
     def last_outcome_success(self):
-        """
+        '''
         Convenience method for determining the success of the last
         OutcomeRequest.
-        """
+        '''
         return all((self.last_outcome_request,
                     self.last_outcome_request.was_outcome_post_successful()))
 
     def build_return_url(self):
-        """
+        '''
         If the Tool Consumer sent a return URL, add any set messages to the
         URL.
-        """
+        '''
         if not self.launch_presentation_return_url:
             return None
 
@@ -196,23 +198,23 @@ class ToolProvider(LaunchParamsMixin, RequestValidatorMixin, object):
 
 
 class DjangoToolProvider(DjangoRequestValidatorMixin, ToolProvider):
-    """
+    '''
     OAuth ToolProvider that works with Django requests
-    """
+    '''
 
     def success_redirect(self, msg='', log=''):
-        """
+        '''
         Shortcut for redirecting Django view to LTI Consumer with messages
-        """
+        '''
         from django.shortcuts import redirect
         self.lti_msg = msg
         self.lti_log = log
         return redirect(self.build_return_url())
 
     def error_redirect(self, errormsg='', errorlog=''):
-        """
+        '''
         Shortcut for redirecting Django view to LTI Consumer with errors
-        """
+        '''
         from django.shortcuts import redirect
         self.lti_errormsg = errormsg
         self.lti_errorlog = errorlog
@@ -220,9 +222,9 @@ class DjangoToolProvider(DjangoRequestValidatorMixin, ToolProvider):
 
 
 class FlaskToolProvider(FlaskRequestValidatorMixin, ToolProvider):
-    """
+    '''
     OAuth ToolProvider that works with Flask requests
-    """
+    '''
     pass
 
 
